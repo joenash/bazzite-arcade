3a5046fc0067b9c4ba33b496ad3fe28cc098042e
Joe Nash <1790822+joenash@users.noreply.github.com>
2026-06-26 16:40:16 +0200

    improved workflows, launcher restart

--- system_files/usr/bin/start-launcher.sh
+++ system_files/usr/bin/start-launcher.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+
+steam -silent &
+sleep 3
+
 # Move to the directory so Godot can find its .pck file relative to the binary
 cd /usr/lib/launcher/
 
-# Launch the Godot app in native fullscreen
-./emf_arcade_launcher.x86_64 --fullscreen
+while true; desktop
+    echo "Starting EMF Arcade Launcher"
+
+    ./emf_arcade_launcher.x86_64 --resolution 1920x1080
+
+    GAME_EXIT_STATUS=$?
+
+    if [ $GAME_EXIT_STATUS -eq 100 ]; then
+        echo "Maintenance mode. Exiting launcher."
+        break
+    fi
+    
+    # Crash or accidental exit
+    echo "Launcher stopped unexpectedly with code $GAME_EXIT_STATUS. Restarting in 5s..."
+    sleep 5
+done
