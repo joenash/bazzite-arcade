70318802be7e554b0299851808ece79dd7c5233c
Joe Nash <1790822+joenash@users.noreply.github.com>
2026-06-27 20:56:18 +0200

    add tailscale and admin config

--- system_files/usr/bin/start-launcher.sh
+++ system_files/usr/bin/start-launcher.sh
@@ -4,7 +4,12 @@
 #sleep 3
 
 LAUNCHER_BIN="/usr/lib/launcher/emf_arcade_launcher.x86_64"
- 
+
+if [[ -f /etc/arcade/setup-complete ]]; then
+    echo "First setup: not booting launcher"
+    exit 0
+fi
+
 if [[ ! -x "$LAUNCHER_BIN" ]]; then
     echo "ERROR: Launcher binary not found or not executable: $LAUNCHER_BIN" >&2
     exit 1
@@ -14,4 +19,3 @@ while true; do
     "$LAUNCHER_BIN" --fullscreen || true
     sleep 2
 done
- 
